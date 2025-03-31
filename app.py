05d6257f4b44d9d08dabea4b2c608865aae560da
Sergei Glazov <s.glazov@innopolis.university>
2025-04-01 01:26:50 +0800

    Fix/add Dockerfile

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Example docker")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Railway!"
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
